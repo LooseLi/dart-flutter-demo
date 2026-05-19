4562d6974cfa760ef4089b99988cc9c92079ccf2
LooseLi <1329307562@qq.com>
2026-05-19 15:49:08 +0800

    feat: flutter

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -11,147 +11,89 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-// ListView默认构建，和SingleChildScrollView一致
-// ListView.builder，按需加载；ListView.separated，和ListView.builder一致，多了构建分割线能力
+// GridView
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('ListView')),
-        body: ListView.separated(
-          separatorBuilder: (context, index) {
-            return Container(
-              width: double.infinity,
-              height: 10,
-              color: Colors.yellow,
-            );
-          },
-          itemCount: 50,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              padding: EdgeInsets.all(20),
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: Text(
-                '${index + 1}',
-                style: TextStyle(color: Colors.white),
+        appBar: AppBar(title: Text('CustomScrollView')),
+        body: CustomScrollView(
+          slivers: [
+            // SliverToBoxAdapter包裹普通widget
+            SliverToBoxAdapter(
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.blue,
+                alignment: Alignment.center,
+                child: Text(
+                  '轮播图',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
               ),
-            );
-          },
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 10)),
+            // 粘性吸顶分类
+            SliverPersistentHeader(delegate: _Sticky(), pinned: true),
+            SliverToBoxAdapter(child: SizedBox(height: 10)),
+            SliverList.separated(
+              itemCount: 50,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  width: double.infinity,
+                  height: 50,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Text(
+                    '${index + 1}',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(height: 10);
+              },
+            ),
+          ],
         ),
-        // body: ListView.builder(
-        //   itemCount: 50,
-        //   itemBuilder: (BuildContext context, int index) {
-        //     return Container(
-        //       margin: EdgeInsets.only(bottom: 20),
-        //       padding: EdgeInsets.all(20),
-        //       color: Colors.blue,
-        //       alignment: Alignment.center,
-        //       child: Text(
-        //         '${index + 1}',
-        //         style: TextStyle(color: Colors.white),
-        //       ),
-        //     );
-        //   },
-        // ),
-        // body: ListView(
-        //   children: List.generate(50, (index) {
-        //     return Container(
-        //       margin: EdgeInsets.only(bottom: 20),
-        //       padding: EdgeInsets.all(20),
-        //       color: Colors.blue,
-        //       alignment: Alignment.center,
-        //       child: Text(
-        //         '${index + 1}',
-        //         style: TextStyle(color: Colors.white),
-        //       ),
-        //     );
-        //   }),
-        // ),
       ),
     );
   }
 }
 
-// SingleChildScrollView滚动组件，只能包含一个子组件
-// 控制滚动，绑定ScrollController对象给controller，使用jumpTo/animateTo方法控制滚动
-// 一次性构建所有子组件，会有性能问题，建议使用ListView
-// class _MyAppState extends State<MyApp> {
-//   final ScrollController _controller = ScrollController(); // 滚动条控制器
+class _Sticky extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            width: 100,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            color: Colors.blue,
+            alignment: Alignment.center,
+            child: Text('${index + 1}', style: TextStyle(color: Colors.white)),
+          );
+        },
+      ),
+    );
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text('ListView')),
-//         body: Stack(
-//           children: [
-//             SingleChildScrollView(
-//               controller: _controller,
-//               padding: EdgeInsets.all(20),
-//               child: Column(
-//                 children: List.generate(50, (index) {
-//                   return Container(
-//                     margin: EdgeInsets.only(bottom: 10),
-//                     width: double.infinity,
-//                     height: 100,
-//                     alignment: Alignment.center,
-//                     color: Colors.blue,
-//                     child: Text(
-//                       '${index + 1}',
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                   );
-//                 }),
-//               ),
-//             ),
-//             Positioned(
-//               top: 10,
-//               right: 10,
-//               child: GestureDetector(
-//                 onTap: () {
-//                   // _controller.jumpTo(_controller.position.maxScrollExtent);
-//                   _controller.animateTo(
-//                     _controller.position.maxScrollExtent,
-//                     duration: Duration(seconds: 1),
-//                     curve: Curves.bounceIn,
-//                   ); // 有动画跳转
-//                 },
-//                 child: Container(
-//                   width: 60,
-//                   height: 60,
-//                   alignment: Alignment.center,
-//                   decoration: BoxDecoration(
-//                     color: Colors.red,
-//                     borderRadius: BorderRadius.circular(30),
-//                   ),
-//                   child: Text('去底部', style: TextStyle(color: Colors.white)),
-//                 ),
-//               ),
-//             ),
-//             Positioned(
-//               bottom: 10,
-//               right: 10,
-//               child: GestureDetector(
-//                 onTap: () {
-//                   _controller.jumpTo(0);
-//                 },
-//                 child: Container(
-//                   width: 60,
-//                   height: 60,
-//                   alignment: Alignment.center,
-//                   decoration: BoxDecoration(
-//                     color: Colors.red,
-//                     borderRadius: BorderRadius.circular(30),
-//                   ),
-//                   child: Text('去顶部', style: TextStyle(color: Colors.white)),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  double get maxExtent => 80; // 最大展开高度
+
+  @override
+  double get minExtent => 40; // 最小折叠高度
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false; // 不需要重建
+  }
+}
---
+++ flutter_base/lib/main05.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+// GridView
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListView')),
+        body: GridView.builder(
+          // 按照列数固定
+          // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //   mainAxisSpacing: 10,
+          //   crossAxisSpacing: 10,
+          //   crossAxisCount: 4,
+          // ),
+          // 最大宽度或者高度
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 2, // 宽高比
+          ),
+          itemCount: 50,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                '${index + 1}',
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          },
+        ),
+        // body: GridView.extent(
+        //   // scrollDirection: Axis.horizontal, // 主轴方向
+        //   maxCrossAxisExtent: 200,
+        //   mainAxisSpacing: 10,
+        //   crossAxisSpacing: 10,
+        //   children: List.generate(50, (index) {
+        //     return Container(
+        //       alignment: Alignment.center,
+        //       color: Colors.blue,
+        //       child: Text(
+        //         '${index + 1}',
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //     );
+        //   }),
+        // ),
+        // body: GridView.count(
+        //   // scrollDirection: Axis.horizontal, // 主轴方向
+        //   crossAxisCount: 3, // 设置固定的列数或者行数
+        //   mainAxisSpacing: 10,
+        //   crossAxisSpacing: 10,
+        //   children: List.generate(50, (index) {
+        //     return Container(
+        //       alignment: Alignment.center,
+        //       color: Colors.blue,
+        //       child: Text(
+        //         '${index + 1}',
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //     );
+        //   }),
+        // ),
+      ),
+    );
+  }
+}
+
+// ListView默认构建，和SingleChildScrollView一致
+// ListView.builder，按需加载；ListView.separated，和ListView.builder一致，多了构建分割线能力
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('ListView')),
+//         body: ListView.separated(
+//           separatorBuilder: (context, index) {
+//             return Container(
+//               width: double.infinity,
+//               height: 10,
+//               color: Colors.yellow,
+//             );
+//           },
+//           itemCount: 50,
+//           itemBuilder: (BuildContext context, int index) {
+//             return Container(
+//               padding: EdgeInsets.all(20),
+//               color: Colors.blue,
+//               alignment: Alignment.center,
+//               child: Text(
+//                 '${index + 1}',
+//                 style: TextStyle(color: Colors.white),
+//               ),
+//             );
+//           },
+//         ),
+//         // body: ListView.builder(
+//         //   itemCount: 50,
+//         //   itemBuilder: (BuildContext context, int index) {
+//         //     return Container(
+//         //       margin: EdgeInsets.only(bottom: 20),
+//         //       padding: EdgeInsets.all(20),
+//         //       color: Colors.blue,
+//         //       alignment: Alignment.center,
+//         //       child: Text(
+//         //         '${index + 1}',
+//         //         style: TextStyle(color: Colors.white),
+//         //       ),
+//         //     );
+//         //   },
+//         // ),
+//         // body: ListView(
+//         //   children: List.generate(50, (index) {
+//         //     return Container(
+//         //       margin: EdgeInsets.only(bottom: 20),
+//         //       padding: EdgeInsets.all(20),
+//         //       color: Colors.blue,
+//         //       alignment: Alignment.center,
+//         //       child: Text(
+//         //         '${index + 1}',
+//         //         style: TextStyle(color: Colors.white),
+//         //       ),
+//         //     );
+//         //   }),
+//         // ),
+//       ),
+//     );
+//   }
+// }
+
+// SingleChildScrollView滚动组件，只能包含一个子组件
+// 控制滚动，绑定ScrollController对象给controller，使用jumpTo/animateTo方法控制滚动
+// 一次性构建所有子组件，会有性能问题，建议使用ListView
+// class _MyAppState extends State<MyApp> {
+//   final ScrollController _controller = ScrollController(); // 滚动条控制器
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('ListView')),
+//         body: Stack(
+//           children: [
+//             SingleChildScrollView(
+//               controller: _controller,
+//               padding: EdgeInsets.all(20),
+//               child: Column(
+//                 children: List.generate(50, (index) {
+//                   return Container(
+//                     margin: EdgeInsets.only(bottom: 10),
+//                     width: double.infinity,
+//                     height: 100,
+//                     alignment: Alignment.center,
+//                     color: Colors.blue,
+//                     child: Text(
+//                       '${index + 1}',
+//                       style: TextStyle(color: Colors.white),
+//                     ),
+//                   );
+//                 }),
+//               ),
+//             ),
+//             Positioned(
+//               top: 10,
+//               right: 10,
+//               child: GestureDetector(
+//                 onTap: () {
+//                   // _controller.jumpTo(_controller.position.maxScrollExtent);
+//                   _controller.animateTo(
+//                     _controller.position.maxScrollExtent,
+//                     duration: Duration(seconds: 1),
+//                     curve: Curves.bounceIn,
+//                   ); // 有动画跳转
+//                 },
+//                 child: Container(
+//                   width: 60,
+//                   height: 60,
+//                   alignment: Alignment.center,
+//                   decoration: BoxDecoration(
+//                     color: Colors.red,
+//                     borderRadius: BorderRadius.circular(30),
+//                   ),
+//                   child: Text('去底部', style: TextStyle(color: Colors.white)),
+//                 ),
+//               ),
+//             ),
+//             Positioned(
+//               bottom: 10,
+//               right: 10,
+//               child: GestureDetector(
+//                 onTap: () {
+//                   _controller.jumpTo(0);
+//                 },
+//                 child: Container(
+//                   width: 60,
+//                   height: 60,
+//                   alignment: Alignment.center,
+//                   decoration: BoxDecoration(
+//                     color: Colors.red,
+//                     borderRadius: BorderRadius.circular(30),
+//                   ),
+//                   child: Text('去顶部', style: TextStyle(color: Colors.white)),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
