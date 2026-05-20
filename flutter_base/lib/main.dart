1ac1c56f6cd8fa2de6752dc0603685b95eb04425
LooseLi <1329307562@qq.com>
2026-05-20 14:08:21 +0800

    feat: flutter

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -1,21 +1,76 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  Dio()
-      .get("/channels")
-      .then((res) {
-        print(res);
-      })
-      .catchError((error) {
-        print(error);
-      });
+  runApp(MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+    _getChannels();
+  }
+
+  List _list = [];
+
+  void _getChannels() async {
+    DioUtils util = DioUtils(); // 创建实例化对象
+    Response<dynamic> result = await util.get('/channels');
+    Map<String, dynamic> res = result.data as Map<String, dynamic>;
+    List data = res['data']['channels'] as List;
+    _list = data;
+    setState(() {});
+    // print(_list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Dio')),
+        body: GridView.extent(
+          maxCrossAxisExtent: 150,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 3,
+          children: List.generate(_list.length, (index) {
+            return ChannelItem(item: _list[index]);
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class ChannelItem extends StatelessWidget {
+  final Map<String, dynamic> item;
+  const ChannelItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      alignment: Alignment.center,
+      child: Text(
+        '${item['name'] ?? '未知'}',
+        style: TextStyle(color: Colors.white, fontSize: 16),
+      ),
+    );
+  }
 }
 
 // 封装 dio 工具类
 class DioUtils {
   final Dio _dio = Dio();
   DioUtils() {
-    // _dio.options.baseUrl = 'https://xxx'; // 基础地址
     // _dio.options.connectTimeout = Duration(seconds: 10); // 连接超时
     // _dio.options.sendTimeout = Duration(seconds: 10); // 发送超时
     // _dio.options.receiveTimeout = Duration(seconds: 10); // 接收超时
@@ -52,7 +107,7 @@ class DioUtils {
   }
 
   // get
-  get(String url, {Map<String, dynamic>? params}) {
+  Future<Response<dynamic>> get(String url, {Map<String, dynamic>? params}) {
     return _dio.get(url, queryParameters: params);
   }
 }
