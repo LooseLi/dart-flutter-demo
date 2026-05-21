d1b3868f24e9fc47a0d6271133f758056bd03672
LooseLi <1329307562@qq.com>
2026-05-21 18:13:13 +0800

    feat: flutter

--- flutter_base/lib/components/home/TheSlider.dart
+++ flutter_base/lib/components/home/TheSlider.dart
@@ -1,20 +1,103 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_base/viewmodels/home.dart';
 
 class TheSlider extends StatefulWidget {
-  const TheSlider({super.key});
+  final List<BannerItem> bannerList;
+  const TheSlider({super.key, required this.bannerList});
 
   @override
   State<TheSlider> createState() => _TheSliderState();
 }
 
 class _TheSliderState extends State<TheSlider> {
+  final CarouselSliderController _controller =
+      CarouselSliderController(); // 控制轮播图
+  int currentIndex = 0;
+
+  Widget _getSlider() {
+    final double screenWidth = MediaQuery.of(context).size.width; // 屏幕宽度
+    return CarouselSlider(
+      carouselController: _controller,
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 3),
+        onPageChanged: (index, reason) {
+          currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          height: 50,
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text(
+            '搜索',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _geDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: currentIndex == index ? 40 : 20,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: currentIndex == index
+                      ? Color.fromRGBO(0, 0, 0, 0.3)
+                      : Colors.white,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      height: 200,
-      alignment: Alignment.center,
-      child: Text('轮播图', style: TextStyle(color: Colors.white, fontSize: 16)),
-    );
+    return Stack(children: [_getSlider(), _getSearch(), _geDots()]);
   }
 }
--- flutter_base/lib/pages/home/index.dart
+++ flutter_base/lib/pages/home/index.dart
@@ -4,6 +4,7 @@ import 'package:flutter_base/components/home/TheHot.dart';
 import 'package:flutter_base/components/home/TheMoreList.dart';
 import 'package:flutter_base/components/home/TheSlider.dart';
 import 'package:flutter_base/components/home/TheSuggestion.dart';
+import 'package:flutter_base/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,9 +14,24 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: '1',
+      imgUrl: 'https://img.beingfine.cn/Channel/i_09543310006_opt.png',
+    ),
+    BannerItem(
+      id: '2',
+      imgUrl: 'https://img.beingfine.cn/Channel/i_09543410007_opt.png',
+    ),
+    BannerItem(
+      id: '3',
+      imgUrl: 'https://img.beingfine.cn/Channel/i_09542910004_opt.png',
+    ),
+  ];
+
   List<Widget> _getScrollChildren() {
     return [
-      SliverToBoxAdapter(child: TheSlider()),
+      SliverToBoxAdapter(child: TheSlider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       SliverToBoxAdapter(child: TheCategory()),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
