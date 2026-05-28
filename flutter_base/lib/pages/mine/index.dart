0655d33038ac15e075884489f6ff14b4b8914bec
LooseLi <1329307562@qq.com>
2026-05-28 18:17:04 +0800

    feat: flutter

--- flutter_base/lib/pages/mine/index.dart
+++ flutter_base/lib/pages/mine/index.dart
@@ -3,6 +3,8 @@ import 'package:flutter_base/api/mine.dart';
 import 'package:flutter_base/components/home/TheMoreList.dart';
 import 'package:flutter_base/components/mine/TheGuess.dart';
 import 'package:flutter_base/models/home.dart';
+import 'package:flutter_base/models/user.dart';
+import 'package:flutter_base/stores/token.dart';
 import 'package:flutter_base/stores/user.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
@@ -57,28 +59,85 @@ class _MineViewState extends State<MineView> {
     });
   }
 
+  Widget _getLogout() {
+    return _userController.user.value.id.isNotEmpty
+        ? Container(
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('提示'),
+                      content: Text('确认要退出吗？'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('取消'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await token.removeToken();
+                            _userController.updateUserInfo(
+                              UserInfo.fromJSON({}),
+                            );
+                            Navigator.pop(context);
+                          },
+                          child: Text('确认'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('退出登录', textAlign: TextAlign.end),
+            ),
+          )
+        : Text('');
+  }
+
   Widget _buildHeader() {
-    return Obx(() {
-      return GestureDetector(
-        onTap: () {
-          if (_userController.user.value.id.isEmpty) {
-            Navigator.pushNamed(context, '/login');
-          }
-        },
-        child: Container(
-          width: double.infinity,
-          height: 200,
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: Text(
-            _userController.user.value.id.isNotEmpty
-                ? _userController.user.value.account
-                : '点击登录',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-        ),
-      );
-    });
+    return Container(
+      padding: EdgeInsets.all(24),
+      width: double.infinity,
+      color: Colors.blue,
+      alignment: Alignment.center,
+      child: Row(
+        children: [
+          Obx(() {
+            return CircleAvatar(
+              radius: 26,
+              backgroundImage: _userController.user.value.avatar.isNotEmpty
+                  ? NetworkImage(_userController.user.value.avatar)
+                  : AssetImage('lib/assets/icon-test.jpeg'),
+              backgroundColor: Colors.white,
+            );
+          }),
+          SizedBox(width: 10),
+          Obx(() {
+            return GestureDetector(
+              onTap: () {
+                if (_userController.user.value.id.isEmpty) {
+                  Navigator.pushNamed(context, '/login');
+                }
+              },
+              child: Text(
+                _userController.user.value.id.isNotEmpty
+                    ? _userController.user.value.account
+                    : '点击登录',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            );
+          }),
+          SizedBox(width: 30),
+          Obx(() {
+            return _getLogout();
+          }),
+        ],
+      ),
+    );
   }
 
   final ScrollController _controller = ScrollController();
