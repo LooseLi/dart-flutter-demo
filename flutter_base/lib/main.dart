b3cb4d639d2a9b1c274611c77adc723ac2dfe407
LooseLi <1329307562@qq.com>
2026-05-29 11:01:41 +0800

    feat: flutter

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -4,3 +4,12 @@ import 'package:flutter_base/routes/index.dart';
 void main(List<String> args) {
   runApp(getRootWidget());
 }
+
+/**
+ * 生成调试版，未签名，用于测试
+ * flutter build apk --debug
+ * 
+ * 发布版 apk，需要签名配置
+ * flutter build apk --release
+ * 
+ */
